3d99063ad80edcc43b79bf7889c64ae6f7762c82
김태경/광고플랫폼개발팀 <taekyung@lguplus.co.kr>
2024-04-14 13:22:54 +0900

    stage 개념추가, 클릭시 이미지교체

--- mole/molecatch.py
+++ mole/molecatch.py
@@ -1,4 +1,5 @@
-import pygame, random
+import pygame
+import random
 
 pygame.init()
 # 게임창 옵션
@@ -23,39 +24,63 @@ def tup_r(tup):
 
 bg_img = pygame.image.load("mole_bg.png")
 bg_img = pygame.transform.smoothscale(bg_img, size)
-mole_img = pygame.image.load("mole1.png")
-mole_size = mole_img.get_size()
+mole_img1 = pygame.image.load("mole1.png")
+mole_img2 = pygame.image.load("mole2.png")
+mole_size = mole_img1.get_size()
 mole_size = tup_r((mole_size[0] * 0.6, mole_size[1] * 0.6))
-mole_img = pygame.transform.smoothscale(mole_img, mole_size)
+mole_img1 = pygame.transform.smoothscale(mole_img1, mole_size)
+mole_img2 = pygame.transform.smoothscale(mole_img2, mole_size)
+mole_img = mole_img1
 
 mole_crop = 0
-mole_move = 5
+mole_move = 10
 mole_stage = 0
+mole_stay_time = 200
+click_go = False
 
-exit = False
+exit_state = False
 
 # 메인 이벤트
-while not exit:
+while not exit_state:
     clock.tick(60)
     # 각종 입력 감지
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            exit = True
+            exit_state = True
         if event.type == pygame.MOUSEBUTTONDOWN:
-            print(pygame.mouse.get_pos())
+            click_go = True
 
+    now_time = pygame.time.get_ticks()
     if mole_stage == 0:
         if random.random() < 0.1:
             mole_stage = 1
     elif mole_stage == 1:
         mole_crop += mole_move
         if mole_crop >= mole_size[1]:
-            mole_stage = 2
             mole_crop = mole_size[1]
+            mole_stage = 2
+            mole_stay_start = now_time
+    elif mole_stage == 2:
+        if now_time - mole_stay_start > mole_stay_time:
+            mole_stage = 3
+    elif mole_stage == 3:
+        mole_crop -= mole_move
+        if mole_crop <= 0:
+            mole_crop = 0
+            mole_stage = 0
+            mole_img = mole_img1
 
     mole_img_cropped = mole_img.subsurface((0, 0, mole_size[0], mole_crop))
     mole_pos = tup_r((x_list[0] - mole_size[0] / 2, y_list[0] - mole_crop))
-
+    mole_range = (mole_pos[0], mole_pos[1], mole_size[0], mole_crop)
+    if click_go == True:
+        x, y = pygame.mouse.get_pos()
+        x1, y1, w, h = mole_range
+        if x >= x1 and x <= x1 + w and y >= y1 and y <= y1+h:
+            # 이미지가 내려가야됨(stage = 3), 이미지 바꾸기(맞은 이미지)
+            mole_stage = 3
+            mole_img = mole_img2
+        click_go = False
     # 그리기
     screen.blit(bg_img, (0, 0))
     screen.blit(mole_img_cropped, mole_pos)
