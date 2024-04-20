39c9e5e60a8d8a7e4a4017dc2799e6c97c42de26
김태경/광고플랫폼개발팀 <taekyung@lguplus.co.kr>
2024-04-20 14:22:51 +0900

    효과음 추가, 맞았을때 이미지 변경

--- bird/bird.py
+++ bird/bird.py
@@ -40,6 +40,8 @@ bird_img_list = [img_read("bird1", 0.5), img_read("bird2", 0.5), img_read("bird3
                  img_read("bird4", 0.5), img_read("bird5", 0.5), img_read("bird6", 0.5)]
 
 dung_img = img_read("dung", 1)
+man_naked_img = img_read("man_naked", 1)
+
 
 class Bird:
     def __init__(self):
@@ -61,7 +63,7 @@ class Bird:
         self.timer = 0
         self.any_move = 0.1
 
-        self.angle = math.atan(abs(self.dy/self.dx)) * 180 / math.pi  # 새의 날아 가는 각도 계산
+        self.angle = math.atan(abs(self.dy / self.dx)) * 180 / math.pi  # 새의 날아 가는 각도 계산
 
         self.drop_x = random.randrange(100, size[0] - 100 - self.size[0])
         self.drop = False
@@ -92,18 +94,19 @@ class Person:
 
 
 class Dung:
-    def __init__(self, x, y, target_x):
+    def __init__(self, x_pos, y_pos, target_x):
         self.img = dung_img
+        self.size = self.img.get_size()
 
         self.y_speed = 3
         self.acceleration = 0.1
 
         self.target_x = target_x
         self.target_y = size[1]
-        self.dx = self.target_x - x
-        self.dy = self.target_y - y
+        self.dx = self.target_x - x_pos
+        self.dy = self.target_y - y_pos
         self.dd = (self.dx ** 2 + self.dy ** 2) ** 0.5  # 빗변(피타고라스 정리)
-        self.pos = x, y
+        self.pos = x_pos, y_pos
         self.move = random.randrange(3, 10)
         self.x_speed = self.move * self.dx / self.dd
 
@@ -119,11 +122,14 @@ right_go = False
 
 exit_state = False
 game_point = 0
-game_time = 50000
+game_time = 10000
 game_over = False
+hit_time = 0
 
 point_font = pygame.font.Font("/System/Library/Fonts/Supplemental/PartyLET-plain.ttf", 60)
 final_font = pygame.font.Font("/System/Library/Fonts/Supplemental/Courier New.ttf", 80)
+man_scream_sound = pygame.mixer.Sound("sound/man-scream.ogg")
+man_scream_sound.set_volume(0.2)
 
 # 메인 이벤트
 game_start_tme = pygame.time.get_ticks()
@@ -155,6 +161,7 @@ while not exit_state:
         bird = Bird()
         bird_list.append(bird)
 
+    # 플레이어 움직임
     if left_go is True and right_go is False:
         player.pos = (player.pos[0] - player.move, player.pos[1])
         player.timer += player.any_move
@@ -168,7 +175,11 @@ while not exit_state:
             player.pos = (size[0] - player.size[0], player.pos[1])
         player.img = p_right_list[int(player.timer) % len(p_right_list)]
     else:
-        player.img = person_static
+        if hit_time > 0:
+            player.img = man_naked_img
+            hit_time -= 1
+        else:
+            player.img = person_static
 
     if remain_time <= 0:
         remain_time = 0
@@ -180,18 +191,16 @@ while not exit_state:
         bird.img = bird_img_list[int(bird.timer) % len(bird_img_list)]
         bird.pos = (bird.pos[0] + bird.x_speed, bird.pos[1] + bird.y_speed)
 
-        if bird.x_speed  > 0:    # 왼쪽에서 오른쪽
-            if bird.pos[0] >= bird.drop_x and bird.drop == False:
+        if bird.x_speed > 0:  # 왼쪽에서 오른쪽
+            if bird.pos[0] >= bird.drop_x and bird.drop is False:
                 dung = Dung(bird.pos[0] + bird.size[0] / 2, bird.pos[1] + bird.size[1], player.pos[0])
                 dung_list.append(dung)
                 bird.drop = True
         else:
-            if bird.pos[0] <= bird.drop_x and bird.drop == False:
+            if bird.pos[0] <= bird.drop_x and bird.drop is False:
                 dung = Dung(bird.pos[0] + bird.size[0] / 2, bird.pos[1] + bird.size[1], player.pos[0])
                 dung_list.append(dung)
                 bird.drop = True
-
-
 
     # 그리기
     screen.fill(white)
@@ -200,10 +209,25 @@ while not exit_state:
     for bird in bird_list:
         bird.show()
 
+    # 새똥 움직임
     for dung in dung_list:
         dung.y_speed += dung.acceleration
         dung.pos = (dung.pos[0] + dung.x_speed, dung.pos[1] + dung.y_speed)
+        X, Y = player.pos
+        W, H = player.size
+        x, y = dung.pos
+        w, h = dung.size
+        if X - w < x < X + w and Y - h < y < Y + H - h:
+            if game_over is not True:
+                game_point -= 100
+            hit_time = 10
         dung.show()
+
+    if game_over is not True:
+        game_point += 1
+
+    if hit_time == 10:
+        man_scream_sound.play()
 
     # 새, 새똥 소멸
     for i, bird in enumerate(bird_list):
