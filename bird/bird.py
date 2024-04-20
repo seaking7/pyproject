34905db71757f65eed57d345343e2e4ec78f1db0
김태경/광고플랫폼개발팀 <taekyung@lguplus.co.kr>
2024-04-20 11:50:38 +0900

    Bird 클래스 생성

--- bird/bird.py
+++ bird/bird.py
@@ -34,19 +34,46 @@ person_size = person_static.get_size()
 p_left_list = [img_read("man_left1", 1), img_read("man_left2", 1), img_read("man_left3", 1)]
 p_right_list = [img_read("man_right1", 1), img_read("man_right2", 1), img_read("man_right3", 1)]
 
-class person:
+bird_list = [img_read("bird1", 0.5), img_read("bird2", 0.5), img_read("bird3", 0.5)]
+
+
+class Bird:
     def __init__(self):
-        self.img = person_static
+        self.img = bird_list[0]
         self.size = self.img.get_size()
-        self.pos = (size[0] / 2 - self.size[0] / 2, size[1] - self.size[1])
-        self.move = 10
+        self.create_x = -self.size[0]
+        self.create_y = 400
+        self.target_x = size[0]
+        self.target_y = 100
+        self.dx = self.target_x - self.create_x
+        self.dy = self.target_y - self.create_y
+        self.dd = (self.dx ** 2 + self.dy ** 2) ** 0.5  # 빗변(피타고라스 정리)
+        self.pos = (self.create_x, self.create_y)
+        self.move = 5
+        self.move_x = self.move * self.dx / self.dd
+        self.move_y = self.move * self.dy / self.dd
+        if self.move_x < 0:
+            self.img = pygame.transform.flip(self.img, True, False)
 
     def show(self):
         screen.blit(self.img, self.pos)
 
 
-player = person()
-k = 0
+class Person:
+    def __init__(self):
+        self.img = person_static
+        self.size = self.img.get_size()
+        self.pos = (size[0] / 2 - self.size[0] / 2, size[1] - self.size[1])
+        self.move = 10
+        self.timer = 0
+        self.any_move = 0.2
+
+    def show(self):
+        screen.blit(self.img, self.pos)
+
+
+player = Person()
+bird = Bird()
 left_go = False
 right_go = False
 
@@ -74,36 +101,39 @@ while not exit_state:
                 right_go = True
         if event.type == pygame.KEYUP:
             key_name = pygame.key.name(event.key)
+            player.timer = 0
             if key_name == "left":
                 left_go = False
             elif key_name == "right":
                 right_go = False
 
-
-    k += 0.2
     now_time = pygame.time.get_ticks()
     remain_time = round((game_time - (now_time - game_start_tme)) / 1000)
 
-    kk = int(k) % 3
-
-
-    if left_go == True and right_go == False:
+    if left_go is True and right_go is False:
         player.pos = (player.pos[0] - player.move, player.pos[1])
-        if player.pos[0] <= 0: player.pos = (0, player.pos[1])
-        player.img = p_left_list[kk]
-    elif left_go == False and right_go == True:
+        player.timer += player.any_move
+        if player.pos[0] <= 0:
+            player.pos = (0, player.pos[1])
+        player.img = p_left_list[int(player.timer) % len(p_left_list)]
+    elif left_go is False and right_go is True:
         player.pos = (player.pos[0] + player.move, player.pos[1])
-        if player.pos[0] >= size[0]-player.size[0]: player.pos = (size[0]-player.size[0], player.pos[1])
-        player.img = p_right_list[kk]
-
+        player.timer += player.any_move
+        if player.pos[0] >= size[0] - player.size[0]:
+            player.pos = (size[0] - player.size[0], player.pos[1])
+        player.img = p_right_list[int(player.timer) % len(p_right_list)]
+    else:
+        player.img = person_static
 
     if remain_time <= 0:
         remain_time = 0
         game_over = True
 
+    bird.pos = (bird.pos[0] + bird.move_x, bird.pos[1] + bird.move_y)
     # 그리기
     screen.fill(white)
     player.show()
+    bird.show()
 
     # 점수표시
     point = point_font.render(f"Score : {game_point}", True, black)
@@ -117,7 +147,7 @@ while not exit_state:
     remain_time_pos = tup_r((size[0] - remain_time_size[0] - 10, 10))
     screen.blit(remain_time, remain_time_pos)
 
-    if game_over == True:
+    if game_over:
         finish_bg = pygame.Surface(size)
         finish_bg.fill(black)
         finish_bg.set_alpha(200)
