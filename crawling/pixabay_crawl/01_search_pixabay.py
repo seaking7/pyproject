8167dce5d471faca3c319079deee477571beefde
김태경/광고플랫폼개발팀 <taekyung@lguplus.co.kr>
2024-04-20 17:20:55 +0900

    픽사베이에서 이미지 다운받기

---
+++ crawling/pixabay_crawl/01_search_pixabay.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+import chromedriver_autoinstaller
+from time import sleep
+
+from urllib.request import Request, urlopen
+
+
+from selenium.webdriver.common.by import By
+
+chromedriver_autoinstaller.install()
+driver = webdriver.Chrome()
+driver.implicitly_wait(5)
+
+url = "https://pixabay.com/ko/images/search/사과/"
+driver.get(url=url)
+
+
+image_xpath = '/html/body/div[1]/div[1]/div/div[2]/div[3]/div/div/div[2]/div[1]/div/a/img'
+
+image_url = driver.find_element(By.XPATH, image_xpath).get_attribute('src')
+print("image_url:", image_url)
+
+image_byte = Request(image_url, headers={'User-Agent': 'Mozilla/5.0'})
+f = open("apple.jpg", "wb")
+f.write(urlopen(image_byte).read())
+f.close()
+
+
+sleep(30)
+
